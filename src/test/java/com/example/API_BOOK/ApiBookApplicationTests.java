35bde8e8b5449511c607a6e1da4ca284c54c0dd1
JUNIOR GABRIEL ANTUNES DE SOUZA <junior_ga_souza@estudante.sesisenai.org.br>
2026-03-10 07:32:43 -0300

    Feat: Properties

---
+++ src/test/java/com/example/API_BOOK/ApiBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_BOOK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
