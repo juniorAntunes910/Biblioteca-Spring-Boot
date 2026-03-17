9843daeb957063aef5c1f936fa1f4fd75c44a0d7
JUNIOR GABRIEL ANTUNES DE SOUZA <junior_ga_souza@estudante.sesisenai.org.br>
2026-03-17 13:41:59 -0300

    Feat: get, Post, Patch

--- src/main/java/com/example/API_BOOK/Controller/BookController.java
+++ src/main/java/com/example/API_BOOK/Controller/BookController.java
@@ -1,10 +1,13 @@
 package com.example.API_BOOK.Controller;
 
+import com.example.API_BOOK.Entity.Book;
+import com.example.API_BOOK.Repository.BookRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 
 @RestController
@@ -12,5 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @AllArgsConstructor
 public class BookController {
-    private BookRepository repository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> listarTodos(){
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    public Book salvar(@RequestBody Book book){
+        return bookRepository.save(book);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Book> alternarStatus(@PathVariable Long id){
+        return bookRepository.findById(id).map(book -> {
+            book.setAvaiable(!book.isAvaiable());
+            Book atualizado = bookRepository.save(book);
+            return ResponseEntity.ok(atualizado);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{title}/{new-title}/title")
+    public ResponseEntity<Book> alternarNome(@PathVariable String nome, @PathVariable String novoNome){
+        return bookRepository.findByTitulo(nome).map(book -> {
+            book.setTitle(novoNome);
+            Book atualizado = bookRepository.save(book);
+            return ResponseEntity.ok(atualizado);
+        }).orElse(ResponseEntity.notFound().build());
+    }
 }
---
+++ src/main/java/com/example/API_BOOK/Repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.example.API_BOOK.Repository;
+
+
+import com.example.API_BOOK.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByTitulo(String titulo);
+}
